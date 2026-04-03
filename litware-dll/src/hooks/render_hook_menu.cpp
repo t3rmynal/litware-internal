bb67fa9e09cb8cefe2306c2637f23f0649e2810f
nakanune <woakxow@gmail.com>
2026-04-03 21:33:34 +0200

    release 0.1.4

--- litware-dll/src/hooks/render_hook_menu.cpp
+++ litware-dll/src/hooks/render_hook_menu.cpp
@@ -946,7 +946,7 @@ static void DrawMenu(){
         dl->AddText(fBold, tSize, {pos.x + (size.x - szT.x)*0.5f, midH - szT.y*0.5f},
             WithAlpha(g_pido.textActive, animEased), title);
 
-        const char* verBuf  = "v0.1.3";
+        const char* verBuf  = "v0.1.4";
         const char* dateBuf = "release: 03.04.2026";
         ImVec2 szVer  = fReg->CalcTextSizeA(9.f*s, FLT_MAX, 0.f, verBuf);
         ImVec2 szDate = fReg->CalcTextSizeA(9.f*s, FLT_MAX, 0.f, dateBuf);
